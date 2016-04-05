c9943165adbc7e78f460e171586f406f676fd445
Axellent <axel.sigl@gmail.com>
2016-04-06 00:48:45 +0100

    Initial commit

---
+++ Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameEngine;
+
+namespace NAJ_Lab1
+{
+    class Engine : ECSEngine
+    {
+        public override void InitialiseContent()
+        {
+        }
+
+        public override void Initialise()
+        {
+
+        }
+    }
+}
